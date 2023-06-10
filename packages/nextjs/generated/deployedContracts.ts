4e0e83fe69282337d85806d6ac5b68e1ab1129a9
Daniel Tao <danieltaox@gmail.com>
2023-06-09 21:25:07 -0400

    feat: switch to mumbai

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -1,3 +1,22 @@
-const deployedContracts = null;
+import {
+  Abi,
+} from "abitype";
+
+export type GenericContractsDeclaration = {
+  [key: number]: readonly {
+    name: string;
+    chainId: string;
+    contracts: {
+      [key: string]: {
+        address: string;
+        abi: Abi;
+      };
+    };
+  }[];
+};
+
+const deployedContracts: GenericContractsDeclaration = {
+  
+};
 
 export default deployedContracts;
--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,22 +1,22 @@
-import Head from "next/head";
+// import Head from "next/head";
 import type { NextPage } from "next";
-import { ContractData } from "~~/components/example-ui/ContractData";
-import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
+// import { ContractData } from "~~/components/example-ui/ContractData";
+// import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Scaffold-ETH 2 Example Ui</title>
         <meta name="description" content="Created with 🏗 scaffold-eth-2" />
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
-      </Head>
+      {/* </Head>
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
         <ContractInteraction />
         <ContractData />
-      </div>
+      </div> */}
     </>
   );
 };
